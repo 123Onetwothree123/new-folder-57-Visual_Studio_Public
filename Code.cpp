939ce841564d5cc7e5ac6db78893442dad476c69
123 <alsttabblt@outlook.com>
2022-07-19 23:13:58 +0800

    123

--- Code.cpp
+++ Code.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <cstring>
 int main(void) {
+	/*
 	printf("HelloWorld.\n");
 	printf("--------------------------------\n");
 	printf("HelloWorld.\n");
 	printf("--------------------------------\n");
 	printf("HelloWorld.\n");
 	printf("--------------------------------\n");
+	*/
+	printf("This is a test.");
+
 	return 0;
 }
