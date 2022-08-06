7d57593f0bbb4f1c725592190a1cd77955e214fb
123 <alsttabblt@outlook.com>
2022-08-06 09:18:52 +0800

    123

--- Code.cpp
+++ Code.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
-#include <cstdio>
-#include <cmath>
-#include <string>
-#include <cstring>
-void Test(int n) {
-	for (int i = 0; i < n; i++)
-	{
-		printf("This is a  test.\n");
-	}
-}
+const int ArSize = 8;
+int sum_arr(const int* begin, const int* end);
 int main(void) {
-	/*
-	printf("HelloWorld.\n");
-	printf("--------------------------------\n");
-	printf("HelloWorld.\n");
-	printf("--------------------------------\n");
-	printf("HelloWorld.\n");
-	printf("--------------------------------\n");
-	*/
-	//Test1:
-	printf("This is a test.\n");
-	printf("Test 1:\n");
-	int n;
-	std::cin >> n;
-	Test(n);
-	printf("--------------------------------\n");
-	printf("Done!\n");
+	int cookies[ArSize] = { 1,2,4,8,16,32,64,128 };
+	int sum = sum_arr(cookies, cookies + ArSize);
+	std::cout << "Total cookies eaten: " << sum << std::endl;
+	sum = sum_arr(cookies, cookies + 3);//firest 3 elements
+	std::cout << "First three eaters ate " << sum << " cookies.\n";
+	sum = sum_arr(cookies + 4, cookies + 8);//last 4 elements
+	std::cout << "Last four eaters ate " << sum << " cookies.\n";
 	return 0;
+}
+int sum_arr(const int* begin, const int* end)
+{
+	const int* pt;
+	int total = 0;
+	for (pt =begin; pt!=end;pt++)
+	{
+		total = total + *pt;
+	}
+	return total;
 }
