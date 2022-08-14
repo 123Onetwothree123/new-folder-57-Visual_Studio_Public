e1cc1706064f84dff4c0ec932b0a53b9a1fe6237
123 <alsttabblt@outlook.com>
2022-08-14 18:03:29 +0800

    123

--- Code.cpp
+++ Code.cpp
@@ -15,7 +15,31 @@ int main(void) {
 	sum = sum_arr(cookies + 4, cookies + 8);		//last 4 elements
 	std::cout << "Last four eaters ate " << sum << " cookies.\n";
 	printf("-------------------------------------------\n");
-
+	double av[3] = { 1112.3,1542.6,2227.9 };
+	const double* (*p1)(const double*, int) = f1;
+	auto p2 = f2;
+	printf("Using pointers to functions:\n");
+	printf(" Address Value\n");
+	std::cout << (*p1)(av, 3) << ": " << *(*p1)(av, 3) << std::endl;
+	std::cout << p2(av, 3) << ": " << *p2(av, 3) << std::endl;
+	const double* (*pa[3])(const double*, int) = { f1,f2,f3 };
+	auto pb = pa;
+	std::cout << "\nUsing an array of pointers to functions:\n";
+	std::cout << " Address Value\n";
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << pa[i](av, 3) << ": " << *pa[i](av, 3) << std::endl;
+	}
+	std::cout << "\nUsing a pointer to a pointer to a function:\n";
+	std::cout << " Address Value\n";
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << pb[i](av, 3) << ": " << *pb[i](av, 3) << std::endl;
+	}
+	std::cout << "\nUsing pointers to an array of pointers:\n";
+	std::cout << " Adress Value\n";
+	auto pc = &pa;
+	std::cout << (*pc)[0](av, 3) << ": " << *(*pc)[0](av, 3) << std::endl;
 	return 0;
 }
 int sum_arr(const int* begin, const int* end)
